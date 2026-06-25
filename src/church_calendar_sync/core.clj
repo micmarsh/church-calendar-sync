1f42f009df5edaa6358a7470d911b78e3fabe72f
Michael Marsh <mike@marsh.pw>
2026-06-25 17:03:49 -0400

    enable pulling vector of vectors

--- src/church_calendar_sync/core.clj
+++ src/church_calendar_sync/core.clj
@@ -4,18 +4,4 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-
-
-(comment
-  (def ^:const ods-file-name "/home/michael/Documents/FrGregoryCalendarjune2026.ods")
-
-  (require '[church-calendar-sync.jopendocument :refer [sheet->clj]])
-
-  (ns church-calendar-sync.jopendocument)
-
-  sheet->clj
-  (use 'clojure.reflect)
-  (reflect sheet)
-
-  )
   
--- src/church_calendar_sync/jopendocument.clj
+++ src/church_calendar_sync/jopendocument.clj
@@ -1,5 +1,10 @@
 (ns church-calendar-sync.jopendocument)
 
+
+(def ^:const relevant-columns 25)
+
+(def ^:const relevant-rows 100)
+
 (defn sheet-from-file-path [ods-file-name]
   (-> ods-file-name
       (java.io.File.)
@@ -10,19 +15,28 @@
   ;; running into issue (with solution) described here https://stackoverflow.com/a/74628786
   (.getValue (.getElement cell)))
 
-(defn- iter-cells [^org.jopendocument.dom.spreadsheet.Sheet sheet]
+;; sheet -> lazy seq of lazy seqs
+(defn sheet->clj [^org.jopendocument.dom.spreadsheet.Sheet sheet]
   (let [row-count (.getRowCount sheet)
         column-count (.getColumnCount sheet)]
-    (for [row-i (range row-count)
-          column-i (range column-count)]
-      {:cell (.getImmutableCellAt sheet column-i row-i)
-       :column column-i
-       :row row-i})))
+    (->> (range row-count)
+         (map (fn [row-i]
+                (->> (range column-count)
+                     (map (fn [column-i]
+                            (let [java-cell (.getImmutableCellAt sheet column-i row-i)]
+                              {:column column-i
+                               :row row-i
+                               :text (cell->text java-cell)
+                               :java-cell java-cell})))))))))
 
-(defn- assoc-cell-text [cell-map]
-  (assoc cell-map :text (-> cell-map :cell cell->text)))
+;; lazy seq of lazy seqs -> trimmed vector of trimmed vectors
+(defn strict [cell-maps]
+  (mapv #(into [] (take relevant-columns) %)
+        (take relevant-rows cell-maps)))
 
-(defn sheet->clj [^org.jopendocument.dom.spreadsheet.Sheet sheet]
-  (->> sheet
-       iter-cells
-       (map assoc-cell-text)))
+
+(comment
+  (def ^:const ods-file-name "/home/michael/Documents/FrGregoryCalendarjune2026.ods")
+
+  (strict (sheet->clj (sheet-from-file-path ods-file-name)))
+  )
