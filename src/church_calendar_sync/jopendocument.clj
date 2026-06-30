c938e8a26c0ea7ce25653eaff3f6a49690e6f63e
Michael Marsh <mike@marsh.pw>
2026-06-30 11:47:50 -0400

    WIP lots of progress towards full parsing to 'domain type'

--- src/church_calendar_sync/jopendocument.clj
+++ src/church_calendar_sync/jopendocument.clj
@@ -1,8 +1,11 @@
 (ns church-calendar-sync.jopendocument
   (:require
-   [clojure.string :as str]
+   [church-calendar-sync.spec :as spec]
+   [clojure.set :as set]
    [clojure.spec.alpha :as s]
-   [church-calendar-sync.spec]))
+   [clojure.string :as str]))
+
+(s/def ::sheet #(instance? org.jopendocument.dom.spreadsheet.Sheet %))
 
 (defn sheet-from-file-path [ods-file-name]
   (-> ods-file-name
@@ -15,72 +18,110 @@
   (.getValue (.getElement cell)))
 
 (defn sheet->clj
-  ([sheet] (sheet->clj {} sheet))
-  ([config ^org.jopendocument.dom.spreadsheet.Sheet sheet]
-   (let [row-count (.getRowCount sheet)
-         column-count (.getColumnCount sheet)]
-     (for [row-i (range (get config :start-row 0) (get config :end-row row-count))
-           column-i (range (get config :start-column 0) (get config :end-column column-count))
-           :let [java-cell (.getImmutableCellAt sheet column-i row-i)]]
-       {:column column-i
-        :row row-i
-        :text (cell->text java-cell)
-        :java-cell java-cell}))))
-
-;; from here down is much more specific to this app rather than jopendocument
-;; probably needs separate ns
-
-;; todo clojure spec! This will likely come from "user space"
-(def ^:const config 
-  {:start-row 10
-   :start-column 0 
-   :day-width 3
-   :day-height 8
-   :end-column 25
-   :end-row 100})
-
-(s/def :config/start-row :grid/row)
-(s/def :config/start-column :grid/column)
-(s/def :config/end-row :grid/row)
-(s/def :config/end-column :grid/column)
-
-(s/def ::config 
-  (s/keys :req-un [:config/start-row :config/end-row :config/start-column :config/end-column]))
-
-(s/def :cell/row :grid/row)
-(s/def :cell/column :grid/column)
-(s/def :cell/text string?)
-
-(s/def ::cell (s/keys :req-un [:cell/column :cell/row :cell/text]))
-
+  [config ^org.jopendocument.dom.spreadsheet.Sheet sheet]
+  (let [row-count (.getRowCount sheet)
+        column-count (.getColumnCount sheet)]
+    (for [row-i (range (get config :start-row 0) (get config :end-row row-count))
+          column-i (range (get config :start-column 0) (get config :end-column column-count))
+          :let [java-cell (.getImmutableCellAt sheet column-i row-i)]]
+      {:column column-i
+       :row row-i
+       :text (cell->text java-cell)
+       :java-cell java-cell})))
 
 (defn- day-start-coords
-  ;; todo this is good motivator: want something that can destructure and run spec at same time?
-  ;; maybe you're still just thinking too "typed"?
-  [{:keys [start-row start-column, day-width day-height] :as config}
-   {:keys [row column] :as cell}]
-  (s/assert ::config config)
-  (s/assert (s/keys :req-un [:cell/column :cell/row]) cell)
+  [{:keys [start-row start-column, day-width day-height]}
+   {:keys [row column]}]
   (let [adjusted-row (- row start-row)
         adjusted-column (- column start-column)]
     [(- adjusted-row (mod adjusted-row day-height))
      (- adjusted-column (mod adjusted-column day-width))]))
 
-(defn group-days [config cell-maps] 
-  (s/assert ::config config)
-  (s/assert (s/every :cell) cell-maps)
+(def ^:private only-group-vals
+  (comp vals (partial sort-by key)))
+
+(def ^:private months 
+  ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"])
+(def ^:private years (mapv str (range 2026 2070)))
+(def ^:private day-of-month? (into #{} (map str (range 1 32))))
+
+(defn- contains-day-of-month? [str]
+  (not-empty (set/intersection day-of-month? (into #{} (str/split str #" |'|,")))))
+
+(defn- contains-year? [str]
+  (some (partial str/includes? str) years))
+
+(defn- contains-month [str]
+  (some (partial str/includes? str) months))
+
+(defn- full-day-str? [str]
+  (and (contains-month str)
+       (contains-year? str)
+       (contains-day-of-month? str)))
+
+(defn- day-group? [group]
+  (let [day-cell-val (:text (first group))]
+    (or (full-day-str? day-cell-val)
+        (day-of-month? day-cell-val))))
+
+(defn- non-sequential? [current-day-val next-day-val]
+  (and (not= (inc current-day-val) next-day-val)
+       (not= 1 next-day-val)
+       (not (#{28, 29, 30, 31} current-day-val))))
+
+(defn- keep-first-continuous [day-groups]
+  (loop [results []
+         remaining (rest day-groups)
+         current-day-val (day-val (first day-groups))]
+    (let [next-day-val (day-val (first remaining))]
+     (cond
+      (empty? remaining) results
+      (non-sequential? current-day-val next-day-val) results
+      :else (recur (conj results (first day-groups))
+                   (rest remaining)
+                   next-day-val)))))
+
+(defn group-days [config cell-maps]
+  (s/assert ::spec/config config)
+  (s/assert (s/+ ::spec/cell) cell-maps)
   (->> cell-maps
        (group-by (partial day-start-coords config))
-       #_(filter day-group?)
-       #_(keep-first-continuous)))
+       (only-group-vals)
+       ;; todo delete, just for testing!
+       (#(concat % [[{:text "22"}] [{:text "23"}] [{:text "24"}] [{:text "25"}]]))
+
+
+       (filter day-group?)
+       (keep-first-continuous)))
 
 (comment
-  (def ^:const ods-file-name "/home/michael/Documents/FrGregoryCalendarjune2026.ods") 
+  (def ^:const ods-file-name "/home/michael/Documents/FrGregoryCalendarjune2026.ods")
 
+
+
+  (def ^:const config
+    {:start-row 10
+     :start-column 0
+     :day-width 3
+     :day-height 8
+     :end-column 25
+     :end-row 100})
+
+  config
+
+  (s/check-asserts true)
   (def sheet (sheet-from-file-path ods-file-name))
 
-  (def days-groups 
-    (->> sheet (sheet->clj config) (group-days {})))
+  (->> sheet
+       (sheet->clj config)
+       (map #(dissoc % :java-cell))
 
-  (map #(dissoc % :java-cell) (days-groups [0 9])) 
+       (group-days config)
+
+       (map first))
+
+  (def days-groups
+    (->> sheet (ods/sheet->clj config) (ods/group-days {})))
+
+  (map #(dissoc % :java-cell) (days-groups [0 9]))
   )
