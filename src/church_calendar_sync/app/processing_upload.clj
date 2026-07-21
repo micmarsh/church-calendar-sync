71e0abf4f72ae592ef395ae275dd1eed6e682757
Michael Marsh <mike@marsh.pw>
2026-07-21 14:25:08 -0400

    WIP and TDD for getting events to sync

--- src/church_calendar_sync/app/processing_upload.clj
+++ src/church_calendar_sync/app/processing_upload.clj
@@ -7,7 +7,8 @@
     [church-calendar-sync.spec :as spec]
     [church-calendar-sync.utils :refer [sort-by-date]]
     [clojure.spec.alpha :as s]
-    [church-calendar-sync.google.gcal :as gcal]))
+    [church-calendar-sync.google.gcal :as gcal]
+    [org.httpkit.server :as server]))
 
 ;; this is copy-paste of `test-config`:
 ;; probably want to do some DI and sharing of some kind of file eventually?
@@ -29,18 +30,91 @@
        ((juxt (comp :event/date-time first) (comp :event/date-time last)))
        ((fn [[start end]] {:start-date start :end-date end}))))
 
-(defn filter-services [existing-events services]
-  {:post [(s/assert (s/coll-of ::spec/service) %)]}
-  nil)
+;; goal?
+;;  START WITH JUST ADDING
+;;  * index "existing" by ':start' DAY (drop timezone?) (ideally maybe can grad tz from calendar for parsing later!??)
+;;  * (mapcat(?) func services)
+;;    * func: looks up whole day "bucket"
+;;      * If there's an event in bucket that matches time and type/description <--> summary
+;;        * return nothing
+;;      * if not, return a new data structure {:add-event {...data...}}
+;;      * ALSO, if feast is missing (service is liturgy and there's not an all day event that matches name)
+;;        also include {:add-feast {....}}
+;;  * actually have above return "start, end, summary"
+;;   
+
+(defn ->date-time [str]
+  (try
+    (java.time.ZonedDateTime/parse str)
+    (catch java.time.format.DateTimeParseException e)))
+
+(defn ->date [str]
+  (try
+    (java.time.LocalDate/parse str)
+    (catch java.time.format.DateTimeParseException e)))
+
+(s/def :google-json/date-time ->date-time)
+(s/def :google-json/time-zone #{"America/New_York" "America/Chicago"})
+(s/def :google-json/zoned-date-time (s/keys :req-un [:google-json/date-time :google-json/time-zone]))
+
+(s/def :google-json/summary string?)
+(s/def :google-json/start :google-json/zoned-date-time)
+(s/def :google-json/end :google-json/zoned-date-time)
+
+(s/def :google-json-full-day/start ->date)
+(s/def :google-json-full-day/end ->date)
+
+(s/def :google-json/event 
+  (s/or :date-time (s/keys :req-un [:google-json/end :google-json/start 
+                                    :google-json/summary])
+        :full-day (s/keys :req-un [:google-json-full-day/start :google-json-full-day/end 
+                                   :google-json/summary])))
+
+(s/def ::events (s/coll-of :google-json/event))
+
+(defn gcal-event-index [events]
+  (s/assert ::events events)
+  (group-by (comp #(.toLocalDate %) #(java.time.ZonedDateTime/parse %) :date-time :start) events))
+
+(defn- matches? [service]
+  (s/assert ::spec/service service)
+  (fn [gcal-json]
+    (s/assert :google-json/event gcal-json)
+    (when-let [date-time-str (-> gcal-json :start :date-time)]
+      (and (= (->date-time date-time-str) (::spec/date-time service))
+           (throw (Exception. "TODO: THIS"))))))
+
+(defn- ->gcal-json-event [service]
+  (throw (Exception. "TODO: THIS")))
+
+(defn- needs-feast? [service day-bucket]
+  (throw (Exception. "TODO: THIS")))
+
+(defn service->gcal-events [existing-events]
+  (s/assert (s/map-of #(instance? java.time.LocalDate %) ::events) existing-events)
+  (fn [service]
+    (s/assert ::spec/service service)
+    (let [day (.toLocalDate (:event/date-time service))
+          day-bucket (get existing-events day)
+          exists? (filter (matches? service) day-bucket)]
+      (filter identity 
+              [(when-not exists? (->gcal-json-event service))
+               (when (needs-feast? service day-bucket) ;; check if service is liturgy and there's no all-day feast event yet
+                 {:start {:date (.format day java.time.format.DateTimeFormatter/ISO_DATE)}
+                  :end {:date (.format day java.time.format.DateTimeFormatter/ISO_DATE)}
+                  :summary (:service/feast service)})]))))
+
+(defn- add-events [calendar-id auth gcal-events]
+  (throw (Exception. "TODO: THIS")))
 
 (defn- sync-calendars [{:keys [token-storage config-storage] :as ctx} services] 
   (s/assert (s/coll-of ::spec/service) services)
   (let [auth (storage/get-token token-storage)
         calendar-id (:id (config/get-config config-storage :church-calendar-sync.app/current-calendar))
         date-range (services-range services)
-        existing-events (-> (gcal/events calendar-id date-range auth) :body :items)]
+        existing-events (-> (gcal/events calendar-id date-range auth) :body :items gcal-event-index)] 
     (->> services
-         (filter-services existing-events)
+         (mapcat (service->gcal-events existing-events))
          (add-events calendar-id auth))))
 
 (defn run [ctx {:keys [params] :as req}]
---
+++ test/church_calendar_sync/unit/calendar_sync_helpers.clj
@@ -0,0 +1,63 @@
+(ns church-calendar-sync.unit.calendar-sync-helpers 
+  (:require [church-calendar-sync.app.processing-upload :refer [gcal-event-index
+                                                                service->gcal-events]]
+            [clojure.spec.alpha :as s]
+            [clojure.test :refer :all]))
+
+
+
+
+
+(def service->gcal-events'
+  (service->gcal-events 
+   (gcal-event-index
+    [{:end {:date-time "2014-07-09T19:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2014-07-09T18:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Akathist for the Ill ~ Акафист для болящих"}
+     {:end {:date-time "2016-09-17T17:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2016-09-17T15:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Fr. Gregory Office Hours by Appointment"}
+     {:end {:date-time "2014-07-12T21:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2014-07-12T18:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Vigil ~ Бдение"}
+     {:end {:date-time "2016-09-24T18:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2016-09-24T17:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Church School -or- Confession ~ Церковная школа -или- Исповедь: calendly.com/ogrisha"}
+     {:end {:date-time "2014-07-06T10:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2014-07-06T09:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Confession by Appointment ~ Исповедь (по предварительной договоренности): calendly.com/ogrisha"}
+     {:end {:date-time "2017-01-01T09:00:00-05:00", :time-zone "America/New_York"},
+      :start {:date-time "2017-01-01T08:00:00-05:00", :time-zone "America/New_York"},
+      :summary "Moleben to St. Vladimir ~ Молебен св. Владимиру"}
+     {:end {:date-time "2014-07-06T13:00:00-04:00", :time-zone "America/New_York"},
+      :start {:date-time "2014-07-06T10:00:00-04:00", :time-zone "America/New_York"},
+      :summary "Sunday Divine Liturgy ~ Воскресная Божественная Литургия"}])))
+
+(deftest test-single-service-gcal-events
+  (s/check-asserts true)
+  (testing "basic mapping of missing service"
+    (is (= [{:end {:date-time "2018-09-16T18:00:00-04:00", :time-zone "America/New_York"},
+             :start {:date-time "2018-09-16T20:00:00-04:00", :time-zone "America/New_York"},
+             :summary "Evening Services"}]
+           (service->gcal-events' {:event-type :event-type/service
+                                   :service/type :service-type/weekday-evening
+                                   :event/date-time (java.time.LocalDateTime/of 2018 9 16 18 0)
+                                   :service/feast "St. Haralambos"}))))
+  (testing "create all-day feast for liturgy"
+    (is (= [{:end {:date-time "2018-09-17T08:00:00-04:00", :time-zone "America/New_York"},
+             :start {:date-time "2018-09-17T10:00:00-04:00", :time-zone "America/New_York"},
+             :summary "Div. Liturgy"}
+            {:end {:date "2018-09-17"},
+             :start {:date "2018-09-17"},
+             :summary "St. Haralambos"}]
+           (service->gcal-events' {:event-type :event-type/service
+                                   :service/type :service-type/liturgy
+                                   :event/date-time (java.time.LocalDateTime/of 2018 9 17 8 0)
+                                   :service/feast "St. Haralambos"}))))
+
+  (testing "skips existing events"
+    (is (empty?
+         (service->gcal-events' {:event-type :event-type/service
+                                 :service/type :service-type/liturgy
+                                 :event/date-time (java.time.LocalDateTime/of 2014 7 6 10 0)
+                                 :service/feast "Sunday ?? After Pentecost"})))))
