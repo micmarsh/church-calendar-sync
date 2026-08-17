98185a6c819a2d1ac1e2c928c120478611a591e8
Michael Marsh <mike@marsh.pw>
2026-08-17 15:40:34 -0400

    trying to get depends correct

--- _build_needs_root.sh
+++ _build_needs_root.sh
@@ -13,12 +13,17 @@ desktop_file=${package_and_binary_name}.desktop
 
 deb_file=$(ls | grep $package_and_binary_name | grep .deb)
 
+control_file_path=${tmp_dir}/DEBIAN/control
+
 dpkg-deb -R $deb_file $tmp_dir
 
 mkdir -p $desktop_file_dir
 mkdir -p $bin_file_dir
 cp $desktop_file  ${desktop_file_dir}/${desktop_file}
 cp $package_and_binary_name ${bin_file_dir}/${package_and_binary_name}
+
+# For some reason jdeb/lein-jdeb is not populating this correctly?
+echo "Depends: libreoffice-common, default-jre | java7-runtime | java6-runtime" >> $control_file_path
 
 dpkg-deb -b $tmp_dir $deb_file
 
